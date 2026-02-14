b1af6409f68a02ea5a5057d7dfc3fe61840fc53f
trentonachico-code <trentonachico@gmail.com>
2026-02-14 03:06:08 -0500

    Revise love messages and celebration title
    Updated love meter messages and celebration emojis.

--- config.js
+++ config.js
@@ -41,16 +41,16 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰😘​💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "WOOOOW You love me that much?? 🥰😘​",  // Shows when they go past 5000%
+        high: "That's right keep going! 💓",              // Shows when they go past 1000%
+        normal: "Uh-huh Uh-huh! 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yippee! I'm the luckiest man in the world! 🎉💝💖💝💓",
+        title: "Yippee! I'm the luckiest man in the world! 🎉💝💓",
         message: "You will be my Valentine forever and always Adele, muuuaaahhhhh!😘​",
-        emojis: "💖🤗💝❤️💕"  // These will bounce around
+        emojis: "💖🤗💝❤️💕🌹😘"  // These will bounce around
     },
 
     // Color scheme for the website
